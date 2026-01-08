f13dbd02e45215cf147e49a78edaf060e8d4ae9a
Dip908 <dipchondo007@gmail.com>
2026-01-08 19:26:48 +0600

    create table and dashboard card component

--- app/Components/HowItWorks/HowItWorksItem.tsx
+++ app/Components/HowItWorks/HowItWorksItem.tsx
@@ -13,8 +13,8 @@ const HowItWorksItem = ({
 }: HowItWorksItemProps) => {
   return (
     <div className="flex flex-col items-center text-center gap-3 px-4 ">
-      <div className="flex items-center justify-center w-14 h-14 rounded-full bg-blue-100">
-        <Icon className="w-7 h-7 text-(--primary)" />
+      <div className="flex items-center justify-center w-14 h-14 rounded-full">
+        <Icon className="w-7 h-7 text-primary" />
       </div>
 
       <h3 className="text-lg font-semibold">{title}</h3>
